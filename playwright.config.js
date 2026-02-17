bc8b55a8cef5e872996ed4480d6b77029fbe7484
nelly <99465502+celciusKE@users.noreply.github.com>
2026-02-18 00:45:58 +0300

    Remove baseURL from Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
 
-     baseURL: 'https://nyansapofoundation-teaching-dashboa.vercel.app/',
+     baseURL: '',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
